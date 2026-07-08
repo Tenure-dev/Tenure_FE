cff652d886a5bb96fb2363a33480592520d0e467
yjudy0531 <yjudy0531@gmail.com>
2026-07-08 11:32:59 +0900

    [feat/#4] 공통 컴포넌트 구현

--- src/app/Router.tsx
+++ src/app/Router.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from 'react-router-dom';
 import FeedPage from '@/pages/home/FeedPage';
 import MyPage from '@/pages/mypage/MyPage';
 import SearchPage from '@/pages/search/SearchPage';
+import TestPage from '@/pages/onboarding/Test';
 
 export const router = createBrowserRouter([
   {
@@ -18,6 +19,7 @@ export const router = createBrowserRouter([
       // ]},
       // 로그인 필요 (ProtectedLayout)
       { path: 'mypage', element: <MyPage /> },
+      { path: 'test', element: <TestPage /> },
     ],
   },
 ]);

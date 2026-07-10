feac42e3ad1c5d63980b4f0d2160aebf04bf72ab
eunhyekimyeah <rladmsgp031219@naver.com>
2026-07-11 00:38:58 +0900

    [feat/#10] 홈에서 사용하는 아이콘 추가

---
+++ src/shared/assets/index.ts
@@ -0,0 +1,6 @@
+// 이 파일은 `pnpm icons`로 자동 생성됩니다. 직접 수정하지 마세요.
+export { default as bellActive } from './bell_active.svg';
+export { default as bellInactive } from './bell_inactive.svg';
+export { default as chevon } from './chevon.svg';
+export { default as naver } from './naver.svg';
+export { default as profileDefault } from './profileDefault.svg';

68836090a44ed2ea2a12104e7a31f257a521cf71
eunhyekimyeah <rladmsgp031219@naver.com>
2026-07-11 00:36:52 +0900

    [feat/#10] 설정 수정

--- src/app/RootLayout.tsx
+++ src/app/RootLayout.tsx
@@ -2,8 +2,8 @@ import { Outlet } from 'react-router-dom';
 
 const RootLayout = () => {
   return (
-    <div className="flex min-h-screen justify-center bg-bg-white">
-      <div className="min-h-screen w-full min-w-[320px] max-w-[768px] bg-bg-white">
+    <div className="bg-bg-white flex min-h-screen justify-center">
+      <div className="bg-bg-white min-h-screen w-full max-w-[768px] min-w-[320px]">
         <Outlet />
       </div>
     </div>

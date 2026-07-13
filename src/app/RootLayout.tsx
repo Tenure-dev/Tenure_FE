c7609d6866d6a9b11c1a3f66962e0a6d235f69e1
KimEunHye <rladmsgp031219@naver.com>
2026-07-13 22:58:31 +0900

    [feat/#21] bottom nav bar ui

--- src/app/RootLayout.tsx
+++ src/app/RootLayout.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from 'react-router-dom';
+import BottomNavBar from '@/shared/components/BottomNavBar';
 
 const RootLayout = () => {
   return (
     <div className="bg-bg-white flex min-h-screen justify-center">
-      <div className="bg-bg-white min-h-screen w-full max-w-[768px] min-w-[320px]">
+      <div className="bg-bg-white min-h-screen w-full max-w-[768px] min-w-[320px] pb-28">
         <Outlet />
+        <BottomNavBar />
       </div>
     </div>
   );

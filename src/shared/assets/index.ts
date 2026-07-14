029a7c0e884b066c6739cb32ef7e9c3962049858
eunhyekimyeah <rladmsgp031219@naver.com>
2026-07-14 14:52:27 +0900

    [feat/#23] 파일 정리

--- src/shared/assets/index.ts
+++ src/shared/assets/index.ts
@@ -1,6 +1,14 @@
 // 이 파일은 `pnpm icons`로 자동 생성됩니다. 직접 수정하지 마세요.
 export { default as bellActive } from './bell_active.svg';
 export { default as bellInactive } from './bell_inactive.svg';
+export { default as bookmark } from './bookmark.svg';
+export { default as buy } from './buy.svg';
 export { default as chevon } from './chevon.svg';
+export { default as edit } from './edit.svg';
+export { default as like } from './like.svg';
 export { default as naver } from './naver.svg';
+export { default as notificationInactive } from './notification_inactive.svg';
 export { default as profileDefault } from './profileDefault.svg';
+export { default as sell } from './sell.svg';
+export { default as settings } from './settings.svg';
+export { default as tagInactive } from './tag_inactive.svg';

c217334c52aaef90b07cabf9bf9d3d42781ad65b
lgdl24 <rkfnal77@skuniv.ac.kr>
2026-07-24 15:20:14 +0900

    [feat/#47] 헤더 알림, 설정 링크 추가

--- src/pages/mypage/component/MyPageHeader.tsx
+++ src/pages/mypage/component/MyPageHeader.tsx
@@ -1,13 +1,19 @@
 import settings from '@/shared/assets/settings.svg';
 import notification from '@/shared/assets/notification_inactive.svg';
+import { useNavigate } from 'react-router-dom';
 
 const MyPageHeader = () => {
+  const navigate = useNavigate();
   return (
     <div className="flex items-center justify-between px-4 py-3">
       <div className="text-title-2">마이페이지</div>
       <div className="flex items-center gap-2">
-        <img src={notification} width={24} height={24} alt="알림" />
-        <img src={settings} width={24} height={24} alt="설정" />
+        <button onClick={() => navigate('/notifications')}>
+          <img src={notification} width={24} height={24} alt="알림" />
+        </button>
+        <button onClick={() => navigate('/settings')}>
+          <img src={settings} width={24} height={24} alt="설정" />
+        </button>
       </div>
     </div>
   );

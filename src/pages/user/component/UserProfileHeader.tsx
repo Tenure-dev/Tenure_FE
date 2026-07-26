db05895bc0f18344fba5c1bffbf6570799bba8af
lgdl24 <rkfnal77@skuniv.ac.kr>
2026-07-26 15:30:14 +0900

    [refactor/#49] 기존 헤더 BackHeader 컴포넌트로 변경

--- src/pages/user/component/UserProfileHeader.tsx
+++ src/pages/user/component/UserProfileHeader.tsx
@@ -1,24 +1,15 @@
-import { useNavigate } from 'react-router-dom';
-import chevronLeft from '@/shared/assets/chevron-left.svg';
+import BackHeader from '@/shared/components/BackHeader';
 import more from '@/shared/assets/more-userprofile.svg';
 
-type Props = {
-  onMoreClick: () => void;
-};
-
-const UserProfileHeader = ({ onMoreClick }: Props) => {
-  const navigate = useNavigate();
-
-  return (
-    <header className="flex items-center justify-between px-4 py-3">
-      <button type="button" onClick={() => navigate(-1)} aria-label="뒤로가기">
-        <img src={chevronLeft} width={24} height={24} alt="" />
-      </button>
+const UserProfileHeader = ({ onMoreClick }: { onMoreClick: () => void }) => (
+  <BackHeader
+    title=""
+    rightActions={
       <button type="button" onClick={onMoreClick} aria-label="더보기">
         <img src={more} width={24} height={24} alt="" />
       </button>
-    </header>
-  );
-};
+    }
+  />
+);
 
 export default UserProfileHeader;

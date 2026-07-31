6cdb5a6c5841d69fdff3ccba2e61f5db268d07d5
KimEunHye <rladmsgp031219@naver.com>
2026-08-01 00:09:35 +0900

    [fix/#127] api 수정

--- src/features/purchase/api/offerApi.ts
+++ src/features/purchase/api/offerApi.ts
@@ -2,7 +2,7 @@ import { api } from '@/shared/lib/api';
 import type { OfferDetailResponse, AcceptTradeResponse } from './dto';
 
 export const getOfferDetail = (offerId: number) =>
-  api.get<OfferDetailResponse>(`/offers/${offerId}`);
+  api.get<OfferDetailResponse>(`/purchase-offers/${offerId}`);
 
 export const acceptOffer = (offerId: number) =>
   api.post<AcceptTradeResponse>(`/purchase-offers/${offerId}/accept`);

0826cc4a60932213ebf0904f0fb874bd39ed5dc8
lgdl24 <rkfnal77@skuniv.ac.kr>
2026-07-31 23:55:16 +0900

    [fix/#133] 충돌 라우터 수정

--- src/app/Router.tsx
+++ src/app/Router.tsx
@@ -40,13 +40,14 @@ import ChatRoomPage from '@/pages/chat/ChatRoomPage';
 import PricePage from '@/pages/purchase/PricePage';
 import CheckoutPage from '@/pages/purchase/CheckoutPage';
 import ProcessingPage from '@/pages/purchase/ProcessingPage';
+import OfferDetailPage from '@/pages/purchase/OfferDetailPage';
+import IntentDetailPage from '@/pages/purchase/IntentDetailPage';
 import OotdTagPage from '@/pages/camera/OotdTagPage';
 import OotdCreatePage from '@/pages/camera/OotdCreatePage';
 import OotdCameraPage from '@/pages/camera/OotdCameraPage';
 import OotdPreviewPage from '@/pages/camera/OotdPreviewPage';
 import UserProfilePage from '@/pages/user/UserProfilePage';
 import ReceivedOffersPage from '@/pages/offer/ReceivedOffersPage';
-
 export const router = createBrowserRouter([
   {
     path: '/',
@@ -57,7 +58,6 @@ export const router = createBrowserRouter([
       { path: 'login', element: <LoginPage /> },
       { path: 'signup', element: <SignupPage /> },
       { path: 'find-password', element: <FindPasswordPage /> },
-
       // 로그인 필요 라우트
       {
         element: <ProtectedLayout />,
@@ -84,6 +84,8 @@ export const router = createBrowserRouter([
           { path: 'users/:userId', element: <UserProfilePage /> },
           { path: 'purchase-history', element: <PurchaseHistoryPage /> },
           { path: 'sales-history', element: <SalesHistoryPage /> },
+          { path: 'purchase/offer/:offerId', element: <OfferDetailPage /> },
+          { path: 'purchase/intent/:intentId', element: <IntentDetailPage /> },
           { path: 'items/:itemId/offers', element: <ReceivedOffersPage /> },
           { path: 'trade/:tradeId', element: <TradeDetailPage /> },
           { path: 'trade/:tradeId/shipping', element: <ShippingInputPage /> },
